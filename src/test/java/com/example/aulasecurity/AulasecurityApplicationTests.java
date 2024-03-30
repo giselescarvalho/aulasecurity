f5d23bc2007426f9b85db53dc605330d8d31ebd2
Gisele <giseledasilvac@gmail.com>
2024-03-30 11:16:38 -0300

    init

---
+++ src/test/java/com/example/aulasecurity/AulasecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aulasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
